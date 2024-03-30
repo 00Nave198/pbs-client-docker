9d11dfa3c89f24196b2d4252faa72386c7a6273d
Evan Masters <00evanmasters@gmail.com>
2024-03-30 15:02:06 -0700

    Entrypoint script now automatically enables specified cron files

--- cron/entrypoint.sh
+++ cron/entrypoint.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 # Cron will read the env from here
 env >> /etc/environment
+
+# Enable provided cronfiles 
+if [ ! -z ${CRON_LIST+x} ]; then
+    IFS=', ' read -r -a CRON_ARRAY <<< "${CRON_LIST}"
+    for element in "${CRON_ARRAY[@]}"
+    do 
+        if [ -f "${element}" ]; then
+            echo "Found cronfile at ${element}"
+            chmod 644 "${element}"
+            crontab "${element}"
+        elif [ -f "/etc/cron.d/${element}" ]; then
+            echo "Found cronfile ${element} in /etc/cron.d/"
+            chmod 644 "/etc/cron.d/${element}"
+            crontab "/etc/cron.d/${element}"
+        fi
+    done
+fi
 
 # Display and execute command
 echo "$@"
